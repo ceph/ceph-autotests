2828e30e1250f373ec8c9020b8da43c9c76aa08d
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-02-23 15:56:11 -0800

    Make cfuse not daemonize.

--- tests/cluster/cluster.py
+++ tests/cluster/cluster.py
@@ -295,7 +295,8 @@ class cluster(test.test):
             fuse = utils.BgJob(
                 # we could use -m instead of ceph.conf, but as we need
                 # ceph.conf to find the keyring anyway, it's not yet worth it
-                command='{bindir}/cfuse -c {conf} --name=client.{id} {mnt}'.format(
+
+                command='{bindir}/cfuse -f -c {conf} --name=client.{id} {mnt}'.format(
                     bindir=ceph_bin,
                     conf=self.ceph_conf.filename,
                     id=id_,
