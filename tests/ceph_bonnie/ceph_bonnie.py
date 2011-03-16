3aaf133e17a8e26f263749012cd05188cd2d8ac0
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-03-16 09:53:16 -0700

    Refactor cfuse_bonnie into ceph_bonnie, can use kclient and cfuse both.

---
+++ tests/ceph_bonnie/ceph_bonnie.py
@@ -0,0 +1,22 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class bonnie(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_bonnie(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            # TODO parallel?
+            mnt = os.path.join(self.tmpdir, 'mnt.{id}'.format(id=id_))
+
+            self.job.run_test(
+                'bonnie',
+                dir=mnt,
+                )
+
+            print 'ceph bonnie test ok'
