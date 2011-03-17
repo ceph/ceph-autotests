a3f050a8c65f7fbd997cf490f94926d7f9b01ad6
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-03-17 12:11:53 -0700

    Add pjd_fstest test.

---
+++ tests/ceph_pjd_fstest/ceph_pjd_fstest.py
@@ -0,0 +1,22 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class ceph_pjd_fstest(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_ceph_pjd_fstest(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            # TODO parallel?
+            mnt = os.path.join(self.tmpdir, 'mnt.{id}'.format(id=id_))
+
+            self.job.run_test(
+                'pjd_fstest',
+                dir=mnt,
+                )
+
+            print 'ceph pjd_fstest test ok'
