83b7942feed7de659aa5bed05f67214303a5a66d
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-03-17 11:09:42 -0700

    Add blogbench test.

---
+++ tests/ceph_blogbench/ceph_blogbench.py
@@ -0,0 +1,22 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class ceph_blogbench(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_ceph_blogbench(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            # TODO parallel?
+            mnt = os.path.join(self.tmpdir, 'mnt.{id}'.format(id=id_))
+
+            self.job.run_test(
+                'blogbench',
+                dir=mnt,
+                )
+
+            print 'ceph blogbench test ok'
