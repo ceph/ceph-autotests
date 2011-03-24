b46ec7564fe5e7a653cc2ce4892ee5ad16327ad9
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-03-24 14:48:02 -0700

    Add ceph_modprobemountloop, like mountloop but also rmmods kernel modules.

---
+++ tests/ceph_modprobemountloop/ceph_modprobemountloop.py
@@ -0,0 +1,21 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class ceph_modprobemountloop(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_ceph_modprobemountloop(self):
+        count = self.extra.get('count', 1000)
+        count = int(count)
+        for i in xrange(count):
+            self.do_902_kernel_unmount()
+            for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+                mnt = os.path.join(self.tmpdir, 'mnt.{id}'.format(id=id_))
+                os.rmdir(mnt)
+            utils.system('rmmod ceph libceph')
+            self.do_072_kernel_mount()
