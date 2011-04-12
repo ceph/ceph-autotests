322ad7df0dc85d733d5ffcee3dbc7aa2897ed48c
Josh Durgin <josh.durgin@dreamhost.com>
2011-04-12 14:30:44 -0700

    Add rbd ffsb test.

---
+++ tests/rbd_ffsb/rbd_ffsb.py
@@ -0,0 +1,22 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class rbd_ffsb(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_rbd_ffsb(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            mnt = os.path.join(self.tmpdir, 'testimage{id}'.format(id=id_))
+
+            self.job.run_test(
+                'ffsb',
+                dir=mnt,
+                profiles=self.extra.get('rbd_ffsb_profiles'),
+                )
+
+            print 'rbd ffsb test ok'
