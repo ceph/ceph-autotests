eb9714c01ade4a3e62dde6a4558cc2436bf52eeb
Josh Durgin <josh.durgin@dreamhost.com>
2011-04-12 14:14:56 -0700

    Add rbd dbench test.

---
+++ tests/rbd_dbench/rbd_dbench.py
@@ -0,0 +1,22 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class rbd_dbench(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_rbd_dbench(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            mnt = os.path.join(self.tmpdir, 'testimage{id}'.format(id=id_))
+
+            self.job.run_test(
+                'dbench',
+                dir=mnt,
+                args=self.extra.get('rbd_dbench_args', ''),
+                )
+
+            print 'rbd dbench test ok'
