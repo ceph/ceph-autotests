4900cfe06c4168dcf3528ab3a1d8ae0572771b81
Josh Durgin <josh.durgin@dreamhost.com>
2011-04-12 14:33:10 -0700

    Add rbd fsx test.

---
+++ tests/rbd_fsx/rbd_fsx.py
@@ -0,0 +1,21 @@
+import os
+
+from autotest_lib.client.bin import utils
+
+from teuthology import ceph
+from teuthology import skeleton
+
+class rbd_fsx(skeleton.CephTest):
+    version = 1
+
+    @skeleton.role('client')
+    def do_150_rbd_fsx(self):
+        for id_ in skeleton.roles_of_type(self.my_roles, 'client'):
+            mnt = os.path.join(self.tmpdir, 'testimage{id}'.format(id=id_))
+
+            self.job.run_test(
+                'fsx',
+                dir=mnt,
+                )
+
+            print 'rbd fsx test ok'
